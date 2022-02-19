96287a366f9f4ed72d55063ff19675a160cf93be
Saurav Sahu <mrsauravsahu@outlook.com>
2022-02-19 13:07:24 +0530

    chore: split tf project and add module for more complex tests

---
+++ playground/t2d2-tf/files.tf
@@ -0,0 +1,3 @@
+module "files" {
+  source = "./modules/files"
+}
--- playground/t2d2-tf/providers.tf
+++ playground/t2d2-tf/providers.tf
@@ -3,7 +3,11 @@ terraform {
     time = {
       version = "0.7.2"
     }
+    local = {
+      version = "2.1.0"
+    }
   }
 }
 
 provider "time" { }
+provider "local" { }
