0570e2cc958b8cf2359120cc8a0a07461b664dfc
Saurav Sahu <mrsauravsahu@outlook.com>
2022-02-24 23:56:12 +0530

    chore: create a jest matchers package for jest integration

---
+++ srv/jest-matchers/src/register-jest-matchers.ts
@@ -0,0 +1,15 @@
+import { ParsedTFPlan } from "t2d2";
+
+const toHaveRootModuleResourceOfType = (plan: ParsedTFPlan, type: string): jest.CustomMatcherResult => {
+  try {
+    plan.hasRootModuleResourceOfType(type)
+    return { pass: true, message: () => `The plan does contains a resource of type '${type}' at the root module.` }
+  }
+  catch (error) {
+    return { pass: false, message: () => (error as Error).message }
+  }
+}
+
+export const registerJestMatchers = () => {
+  expect.extend({ toHaveRootModuleResourceOfType });
+}
