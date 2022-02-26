e0315b2161d6c8a3a76349dcc6590eaea1971b46
Saurav Sahu <mrsauravsahu@outlook.com>
2022-02-26 11:58:44 +0530

    test: add test to pick root module resources

--- svc/core/__tests__/utils/parse-tf-plan.ts
+++ svc/core/__tests__/utils/parse-tf-plan.ts
@@ -14,7 +14,7 @@ describe('parsePlan', () => {
     const parsedTFPlan = await parseTFPlan(input)
     expect(parsedTFPlan.formatVersion).toEqual("1.0.0");
   })
-  
+
   test('should pick terraform version', async () => {
     const input = JSON.stringify({
       terraform_version: '1.1.6'
@@ -22,5 +22,34 @@ describe('parsePlan', () => {
 
     const parsedTFPlan = await parseTFPlan(input)
     expect(parsedTFPlan.tfVersion).toEqual("1.1.6");
+  })
+
+  test('should pick root module resources', async () => {
+    const input = JSON.stringify({
+      terraform_version: '1.1.6',
+      format_version: '1.0.0',
+      planned_values: {
+        root_module: {
+          resources: [
+            {
+              address: "time_sleep.wait_some_time",
+              mode: "managed",
+              type: "time_sleep",
+              name: "wait_some_time",
+              provider_name: "registry.terraform.io/hashicorp/time",
+              schema_version: 0,
+              values: {
+                create_duration: "1s",
+                destroy_duration: null,
+                triggers: null
+              }
+            }
+          ]
+        }
+      }
+    })
+
+    const parsedTFPlan = await parseTFPlan(input)
+    expect(parsedTFPlan.resources.length).toBe(1)
   })
 })
