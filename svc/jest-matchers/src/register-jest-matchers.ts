2f9885d2ec9ea583c319911da65041d19820f51e
Saurav Sahu <mrsauravsahu@outlook.com>
2022-02-28 23:28:25 +0530

    bump: @t2d2/jest-matchers@0.1.0-alpha.2

--- svc/jest-matchers/src/register-jest-matchers.ts
+++ svc/jest-matchers/src/register-jest-matchers.ts
@@ -2,7 +2,7 @@ import { ParsedTFPlan } from "@t2d2/core";
 
 const toHaveRootModuleResourceOfType = (plan: ParsedTFPlan, type: string): jest.CustomMatcherResult => {
   try {
-    plan.hasRootModuleResourceOfType(type)
+    plan.getRootModuleResourceOfType(type)
     return { pass: true, message: () => `The plan does contains a resource of type '${type}' at the root module.` }
   }
   catch (error) {
